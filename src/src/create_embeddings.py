8f2055e4325a98080f2fcd213499f11b7b1c3ace
FrancescoZanella <francescozanella00@gmail.com>
2024-08-21 18:30:29 +0200

    disable tensorflow warnings

--- src/src/create_embeddings.py
+++ src/src/create_embeddings.py
@@ -1,5 +1,6 @@
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 from tensorflow.keras import layers as tfkl
 from tensorflow import keras as tfk
 import tensorflow as tf

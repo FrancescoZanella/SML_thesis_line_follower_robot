4df94c63b6d6336b49330419f7501f054c1fca61
FrancescoZanella <francescozanella00@gmail.com>
2024-08-22 10:05:53 +0200

    added images parameter

--- src/scripts/run_train_streaming_models.bat
+++ src/scripts/run_train_streaming_models.bat
@@ -1,6 +1,6 @@
 @echo off
 python "C:\Users\franc\Desktop\TESI\SML_thesis_line_follower_robot\src\src\train_streaming_models.py" ^
     -output_dir "C:\Users\franc\Desktop\TESI\SML_thesis_line_follower_robot\webots\data\models" ^
-    -dataset_path "C:\\Users\\franc\\Desktop\\TESI\\SML_thesis_line_follower_robot\\webots\\data\\data\\sensors_data\\name.csv" ^
+    -dataset_path "C:\\Users\\franc\\Desktop\\TESI\\SML_thesis_line_follower_robot\\webots\\data\\data\\sensors_data\\prova.csv" ^
     -model_name "hat" ^
-    -evaluate "True"
+    -with_images "True"

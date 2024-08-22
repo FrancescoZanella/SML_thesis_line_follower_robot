72fc23c82d2f3d411c908bd99b21ac5d983012b8
FrancescoZanella <francescozanella00@gmail.com>
2024-08-22 10:47:42 +0200

    create readme

--- src/scripts/run_add_image_embeddings.bat
+++ src/scripts/run_add_image_embeddings.bat
@@ -1,8 +1,8 @@
 @echo off
 python "C:\Users\franc\Desktop\TESI\SML_thesis_line_follower_robot\src\src\create_embeddings.py" ^
-    -output_dir "C:\\Users\\franc\\Desktop\\TESI\\SML_thesis_line_follower_robot\\webots\\data\\data\\sensors_data\\prova.csv" ^
+    -output_dir "C:\\Users\\franc\\Desktop\\TESI\\SML_thesis_line_follower_robot\\webots\\data\\data\\sensors_data\\umap_5_sensor_data 2024-08-21_16-06-41.csv" ^
     -dataset_path "C:\\Users\\franc\\Desktop\\TESI\\SML_thesis_line_follower_robot\\webots\\data\\data\\sensors_data\\sensor_data 2024-08-21_16-06-41.csv" ^
     -images_path "C:\\Users\\franc\\Desktop\\TESI\\SML_thesis_line_follower_robot\\webots\\data\\images\\" ^
     -model_path "C:\\Users\\franc\\Desktop\\TESI\\SML_thesis_line_follower_robot\\webots\\data\\models\\generate_embeddings_images.keras" ^
     -dimensionality_reduction "True" ^
-    -n_components "30"
+    -n_components "5"
--- src/scripts/run_train_streaming_models.bat
+++ src/scripts/run_train_streaming_models.bat
@@ -1,6 +1,6 @@
 @echo off
 python "C:\Users\franc\Desktop\TESI\SML_thesis_line_follower_robot\src\src\train_streaming_models.py" ^
     -output_dir "C:\Users\franc\Desktop\TESI\SML_thesis_line_follower_robot\webots\data\models" ^
-    -dataset_path "C:\\Users\\franc\\Desktop\\TESI\\SML_thesis_line_follower_robot\\webots\\data\\data\\sensors_data\\prova.csv" ^
-    -model_name "hat" ^
-    -with_images "True"
+    -dataset_path "C:\\Users\\franc\\Desktop\\TESI\\SML_thesis_line_follower_robot\\webots\\data\\data\\sensors_data\\sensor_data 2024-08-21_16-06-41.csv" ^
+    -model_name "ht" ^
+    -with_images "False"

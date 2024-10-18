d0f27334399f47250ce3822ab792533ba792036b
FrancescoZanella <francescozanella00@gmail.com>
2024-10-18 14:57:27 +0200

    added data and models

--- src/scripts/run_train_streaming_models_regression.bat
+++ src/scripts/run_train_streaming_models_regression.bat
@@ -1,5 +1,6 @@
 @echo off
 python "C:\Users\franc\Desktop\TESI\SML_thesis_line_follower_robot\src\src\train_streaming_models_regression.py" ^
-    -output_dir "C:\\Users\\franc\\Desktop\\TESI\\SML_thesis_line_follower_robot\\tmp\\e-puck\\data\\models" ^
-    -dataset_path "C:\\Users\\franc\\Desktop\\TESI\\SML_thesis_line_follower_robot\\tmp\\e-puck\\data\\data\\sensors_data\\sensor_with_emb_5.csv" ^
+    -output_dir "C:\\Users\\franc\\Desktop\\TESI\\SML_thesis_line_follower_robot\\e-puck\\data\\models" ^
+    -dataset_path "C:\\Users\\franc\\Desktop\\TESI\\SML_thesis_line_follower_robot\\e-puck\\data\\data\\sensors_data\\sensors_data_2024-10-04_15-59-20.csv" ^
     -model_name "arf"
+
